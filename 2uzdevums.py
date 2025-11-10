3c5a0a4ebb99f19bc56f5baa4af5e06f526fea87
emilsvagelis <emilvage@madona.edu.lv& C:/Users/skolens/AppData/Local/Programs/Python/Python310/python.exe c:/Users/skolens/Desktop/ProgrammesanaEmilsV/25nov/macas_2024/darbs.py>
2025-11-10 12:42:28 +0200

    izpildīts 2.uzd.

--- 2uzdevums.py
+++ 2uzdevums.py
@@ -1,4 +1,4 @@
-  """
+"""
     Funkcija koks akceptē trīs argumentus - skaiļus viens, divi un trīs, 
     aprēķina to summas kvadrātu un atgriež to.
     Pārbaudiet funkcijas darbību ar dažādiem argumentiem, 
@@ -11,3 +11,16 @@
     Atgriež:
         int vai float -- argumentu summa
 """
+
+def koks(a,b,c):
+    summa=a+b+c   # jāiegūst visu sk. summa
+    return summa*summa # summas kvadrāts
+#piemēri visi varianti
+rezultats1=koks(1,2,3) # tiek apskatīti vesali sk.
+rezultats2=koks(2.5,3.5,4.0) # visi sk. ir float
+rezultats3=koks(-1,5,2)
+
+# datu izvads
+print(f"rezultats1: {rezultats1:.2f}")
+print(f"rezultats2: {rezultats2:.2f}")
+print(f"rezultats3: {rezultats3:.2f}")
