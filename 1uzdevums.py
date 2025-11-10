e4978fc48a6fc66ae57dd8de92185bb8f3c3aa2a
emilsvagelis <emilvage@madona.edu.lv& C:/Users/skolens/AppData/Local/Programs/Python/Python310/python.exe c:/Users/skolens/Desktop/ProgrammesanaEmilsV/25nov/macas_2024/darbs.py>
2025-11-10 12:24:55 +0200

    izpildīts 1. uzd

--- 1uzdevums.py
+++ 1uzdevums.py
@@ -4,3 +4,16 @@ Summējot divus norādītos veselos skaitļus.
 Tomēr, ja summa ir no 15 līdz 20, tā atgriezīs 20.
 
 """
+def spec_summa(a,b): # a un b ir parametri
+    kopa=a+b 
+    if 15<= kopa<=20:
+        return 20
+    else:
+        return kopa
+# piemērs 
+sk1=int(input("ievadi 1. skaitli"))
+sk2=int(input("ievadi 2. skaitli"))
+
+rezultats=spec_summa(sk1, sk2) # jānorāda 2 argumenti
+
+print(f"rezultāts ir: {rezultats}")
